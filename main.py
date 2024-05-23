45502945a785dfe471cafce188f52bd6a6e02c7d
pakmarina108@gmail.com <pm@mail.ru>
2024-05-23 20:40:17 +0300

    added to show list overview of all days with number of patients

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 from flaskwebgui import FlaskUI
 from datetime import date
 from flask import Flask, redirect, request, render_template
-import random
 import openpyxl
+
+from models.record import Record
 
 app = Flask(__name__, static_url_path="", static_folder=os.path.join(os.getcwd(), "flask_desktop_app/phrases"))
 
@@ -15,10 +16,17 @@ def home():
     path=os.path.join(os.getcwd(), "records/medical.xlsx")
     wb = openpyxl.load_workbook(path)
     names = wb.sheetnames
-    ws = wb["current"]
     if (day not in names):
         return redirect('/assign')
-    return render_template('start.html',title="Список отчетов", tab = zip(titles), fileNames = zip(names))
+    else:
+        ws = wb["current"]
+        header = [cell.value for cell in ws[1]]
+        records=[]
+        for i in range(2,ws.max_row+1):
+            args =[cell.value for cell in ws[i]]
+            record = Record(*args)
+            records.append(record)
+        return render_template('start.html', title="Список отчетов", tab = header, records = records)
 
 @app.route('/assign')
 def my_form():
@@ -27,7 +35,6 @@ def my_form():
 @app.route('/assign', methods=['POST'])
 def my_form_post():
     text = request.form['text']
-    processed_text = text.upper()
     day = str(date.today())
     path=os.path.join(os.getcwd(), "records/medical.xlsx")
     wb = openpyxl.load_workbook(path)
@@ -36,7 +43,7 @@ def my_form_post():
     ws = wb["current"]
     ws.append(new_data)
     wb.save("records/medical.xlsx")
-    return processed_text
+    return redirect("/")
 
 if __name__ == "__main__":
   FlaskUI(app=app, server="flask",  width= 700, height=700).run()
---
+++ models/record.py
@@ -0,0 +1,5 @@
+class Record():
+    def __init__(self, date=None, doctor=None, numberPatients=None):
+        self.date = date
+        self.doctor = doctor
+        self.patients = numberPatients
