59993fd5dfdf9389de4b41434ec4dbddd97b2f14
pakmarina108@gmail.com <pm@mail.ru>
2024-05-31 13:33:46 +0300

    added logger and exception handler

--- main.py
+++ main.py
@@ -11,113 +11,154 @@ from service.service import Service
 app = Flask(__name__)
 
 directory = os.getcwd()
-
+layer = "Controller"
 @app.route("/")
 def home():
-    day = Service.getDay()
-    wb = Service.getWB()
-    names = wb.sheetnames
-    if (day not in names):
-        return redirect('/assign')
-    else:
-        ws = wb["current"]
-        header = [cell.value for cell in ws[1]]
-        records = Service.countPatients(Service, wb, day)
+    try: 
+        day = Service.getDay()
+        wb = Service.getWB()
+        printErrorInLoggerThrowException(wb)
+        names = wb.sheetnames
+        if (day not in names):
+            app.logger.info('%s : day - %s', layer, day)
+            return redirect('/assign')
+        else:
+            ws = wb["current"]
+            header = [cell.value for cell in ws[1]]
+            records = Service.countPatients(Service, wb, day)
+            printErrorInLoggerThrowException(records)
         
-        return render_template('start.html', title="Список отчетов", tab = header, records = records)
-
+            return render_template('start.html', title="Список отчетов", tab = header, records = records)
+        #raise Exception("error in getWB:")
+    except Exception as e:
+            return redirectToErrorPage(str(e), __name__)
+       
 @app.route('/assign')
 def my_form():
-    day = Service.getDay()
-    wb = Service.getWB()
-    isButtonActive=False
-    ws = wb['settings']
-    doctors = Service.fromExcelToList(Doctor, ws)
-    if (day not in wb.sheetnames):
-        isButtonActive = True
-    return render_template('assign.html', doctors = doctors, isActive = isButtonActive)
-
+    try:
+        day = Service.getDay()
+        wb = Service.getWB()
+        printErrorInLoggerThrowException(wb)
+        isButtonActive=False
+        ws = wb['settings']
+        doctors = Service.fromExcelToList(Doctor, ws)
+        printErrorInLoggerThrowException(doctors)
+        if (day not in wb.sheetnames):
+            isButtonActive = True
+        return render_template('assign.html', doctors = doctors, isActive = isButtonActive)
+    except Exception as e:
+            return redirectToErrorPage(str(e), __name__)
+        
 @app.route('/assign', methods=['POST'])
 def my_form_post():
-    docId = request.form['text']
-    day = Service.getDay()
+    try:
+        docId = request.form['text']
+        day = Service.getDay()
 
-    wb = Service.getWB()
-    
-    ws_new = wb.create_sheet(day) 
-    ws_header = ["Время","ФИО пациента", "М\Ж\Р", "Дата рождения", "Причина", "Давление"]
-    ws_new.append(ws_header)
-    
-    wsDoc = wb["settings"]
-    docName = wsDoc[docId][1].value
-    
-    new_data = [day, docName, -1, docId]
-    ws = wb["current"]
-    ws.append(new_data)
-    wb.save("records/medical.xlsx")
-    return redirect("/")
-
+        wb = Service.getWB()
+        printErrorInLoggerThrowException(wb)
+        ws_new = wb.create_sheet(day) 
+        ws_header = ["Время","ФИО пациента", "М\Ж\Р", "Дата рождения", "Причина", "Давление"]
+        ws_new.append(ws_header)
+        app.logger.info('%s : added worksheet (%s)', layer, day)
+        
+        wsDoc = wb["settings"]
+        docName = wsDoc[docId][1].value
+        
+        new_data = [day, docName, -1, docId]
+        ws = wb["current"]
+        ws.append(new_data)
+        wb.save("records/medical.xlsx")
+        app.logger.info('%s : saved doctor (%s) name - %s', layer, docId, docName)
+        app.logger.info('%s : saved document with new data', layer)
+        return redirect("/")
+    except Exception as e:
+        return  redirectToErrorPage(str(e), __name__)
+        
 @app.route('/day/<id>', defaults={'errid': 0, 'name': None})
 @app.route('/day/<id>/<errid>/<name>')
 def patients(id, errid, name):
-    error = ""
-    wb = Service.getWB()
-    isActiveDay = False
-    if(id == str(date.today())):
-        isActiveDay = True
-    if(errid=="1"):
-        error = "ФИО "+name+" с идентичной датой рождения уже существует. Пожалуйста проверьте еще раз данные."
-    if(id in wb.sheetnames):
-         ws = wb[id]  
-         patients= Service.fromExcelToList(Patient, ws)
-         return render_template('patient.html', 
-                                patients = patients, 
-                                day = id, 
-                                isActive = isActiveDay, 
-                                length = len(patients)+1, 
-                                error = error)
-    else:
-        return redirect("/")
-    
+    try:
+        error = ""
+        wb = Service.getWB()
+        printErrorInLoggerThrowException(wb)
+        isActiveDay = False
+        if(id == Service.getDay):
+            isActiveDay = True
+        if(errid=="1"):
+            error = "ФИО "+name+" с идентичной датой рождения уже существует. Пожалуйста проверьте еще раз данные."
+        if(id in wb.sheetnames):
+            ws = wb[id]  
+            patients= Service.fromExcelToList(Patient, ws)
+            printErrorInLoggerThrowException(patients)
+            return render_template('patient.html', 
+                                    patients = patients, 
+                                    day = id, 
+                                    isActive = isActiveDay, 
+                                    length = len(patients)+1, 
+                                    error = error)
+        else:
+            return redirect("/")
+    except Exception as e:
+            return  redirectToErrorPage(str(e), __name__)
+        
 @app.route('/day/<id>', methods=['POST'])
 def patients_post(id):
-    patient_id = request.form['id']
-    patient_name = request.form['name']
-    patient_type = request.form['type']
-    patient_birthdate = request.form['birthdate']
-    patient_reason = request.form['reason']
-    patient_pressure = request.form['pressure']
-    url = Service.checkSavePatientGetPage(Service,
-                                          id,
-                                          patient_id,
-                                          patient_name,
-                                          patient_type,
-                                          patient_birthdate,
-                                          patient_reason,
-                                          patient_pressure)
-    return redirect(url)
+    try:
+        patient_id = request.form['id']
+        patient_name = request.form['name']
+        patient_type = request.form['type']
+        patient_birthdate = request.form['birthdate']
+        patient_reason = request.form['reason']
+        patient_pressure = request.form['pressure']
+        url = Service.checkSavePatientGetPage(Service,
+                                            id,
+                                            patient_id,
+                                            patient_name,
+                                            patient_type,
+                                            patient_birthdate,
+                                            patient_reason,
+                                            patient_pressure)
+        printErrorInLoggerThrowException(url)
+        return redirect(url)
+    except Exception as e:
+           return  redirectToErrorPage(str(e), __name__)
 
 @app.route('/day/<id>/<errid>/<name>', defaults={'errid': 0, 'name': None},  methods=['POST'])
 def patients_post_error(id):
-    patient_id = request.form['id']
-    patient_name = request.form['name']
-    patient_type = request.form['type']
-    patient_birthdate = request.form['birthdate']
-    patient_reason = request.form['reason']
-    patient_pressure = request.form['pressure']
-    url = Service.checkSavePatientGetPage(Service,
-                                          id,
-                                          patient_id,
-                                          patient_name,
-                                          patient_type,
-                                          patient_birthdate,
-                                          patient_reason,
-                                          patient_pressure)
-    return redirect(url)
+    try:
+        patient_id = request.form['id']
+        patient_name = request.form['name']
+        patient_type = request.form['type']
+        patient_birthdate = request.form['birthdate']
+        patient_reason = request.form['reason']
+        patient_pressure = request.form['pressure']
+        url = Service.checkSavePatientGetPage(Service,
+                                            id,
+                                            patient_id,
+                                            patient_name,
+                                            patient_type,
+                                            patient_birthdate,
+                                            patient_reason,
+                                            patient_pressure)
+        printErrorInLoggerThrowException(url)
+        return redirect(url)
+    except Exception as e:
+            redirectToErrorPage(str(e), __name__)
 
-
-
-
+#################################################################################
+#to print error msg from service layer and then redirect via Exception to error.html page
+def printErrorInLoggerThrowException(variableToCheck):
+    if((type(variableToCheck) is str) and variableToCheck.startswith("error")):
+                app.logger.error('Service msg: %s', variableToCheck)
+                raise Exception
+#to save to logger msg and redirect to error page
+def redirectToErrorPage(msg, name):
+    app.logger.error('%s : in def %s - %s', layer,name, msg)
+    return render_template('error.html')
+################################################################################### 
 if __name__ == "__main__":
-  FlaskUI(app=app, server="flask",  width= 800, height=600).run()
+    import logging   
+    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+    FlaskUI(app=app, server="flask",  width= 800, height=600).run()
   
--- service/service.py
+++ service/service.py
@@ -11,8 +11,11 @@ class Service():
     T = TypeVar('T') 
     
     def getWB():
-        path=os.path.join(os.getcwd(), "records/medical.xlsx")
-        return openpyxl.load_workbook(path)
+        try:
+            path=os.path.join(os.getcwd(), "records/medical.xlsx")
+            return openpyxl.load_workbook(path)
+        except Exception as e:
+            return "error in getWB:"+str(e)
 
     def getDay():
         return str(date.today())
@@ -21,66 +24,77 @@ class Service():
         return datetime.now()
     
     def fromExcelToList(T, ws):
-        listObjects=[]
-        for i in range(2,ws.max_row+1):
-            args =[cell.value for cell in ws[i]]
-            typeObject = T(*args)
-            listObjects.append(typeObject)
-        return listObjects
+        try: 
+            listObjects=[]
+            for i in range(2,ws.max_row+1):
+                args =[cell.value for cell in ws[i]]
+                typeObject = T(*args)
+                listObjects.append(typeObject)
+            return listObjects
+        except Exception as e:
+            return "error in fromExcelToList:"+str(e)
     
     def saveRecord(wb,name, new_data):
-        ws=wb[name]
-        ws.append(new_data)
-        wb.save("records/medical.xlsx")
+        try:
+            ws=wb[name]
+            ws.append(new_data)
+            wb.save("records/medical.xlsx")
+        except Exception as e:
+            return "error in saveRecord:"+str(e)
      
     def countPatients(self, wb, today):
+        try: 
+            ws = wb['current']
+            records = self.fromExcelToList(Record,ws)
         
-        ws = wb['current']
-        records = self.fromExcelToList(Record,ws)
-        
-        for i in  range(2, ws.max_row+1):
-            if(records[i-2].patients == -1 or records[i-2].date == today):
-                day_name = records[i-2].date #name of the day and worksheet name
-                men_num=0
-                woman_num=0
-                child_num=0
-                ws_current = wb[day_name] #get to specific day page
-                patients_num = ws_current.max_row-1 #get number of patients that day
+            for i in  range(2, ws.max_row+1):
+                if(records[i-2].patients == -1 or records[i-2].date == today):
+                    day_name = records[i-2].date #name of the day and worksheet name
+                    men_num=0
+                    woman_num=0
+                    child_num=0
+                    ws_current = wb[day_name] #get to specific day page
+                    patients_num = ws_current.max_row-1 #get number of patients that day
                 
-                records[i-2].patients = patients_num
-                #if there are patients, then count types
-                if(patients_num!=0):
-                    df=pd.read_excel('records/medical.xlsx', i)#count men woman child
-                    mylist = df['М\Ж\Р'].tolist() #get types list
-                    #count each type specifically
-                    woman_num = mylist.count("Ж") 
-                    men_num = mylist.count("М")  
-                    child_num = mylist.count("Р")  
-                    #save to records list
-                    records[i-2].child = child_num
-                    records[i-2].woman = woman_num
-                    records[i-2].men = men_num
-                    #save all info           
-                    ws.cell(row=i, column=3).value = patients_num
-                    ws.cell(row=i, column=5).value = child_num
-                    ws.cell(row=i, column=6).value = woman_num
-                    ws.cell(row=i, column=7).value = men_num
+                    records[i-2].patients = patients_num
+                    #if there are patients, then count types
+                    if(patients_num!=0):
+                        df=pd.read_excel('records/medical.xlsx', i)#count men woman child
+                        mylist = df['М\Ж\Р'].tolist() #get types list
+                        #count each type specifically
+                        woman_num = mylist.count("Ж") 
+                        men_num = mylist.count("М")  
+                        child_num = mylist.count("Р")  
+                        #save to records list
+                        records[i-2].child = child_num
+                        records[i-2].woman = woman_num
+                        records[i-2].men = men_num
+                        #save all info           
+                        ws.cell(row=i, column=3).value = patients_num
+                        ws.cell(row=i, column=5).value = child_num
+                        ws.cell(row=i, column=6).value = woman_num
+                        ws.cell(row=i, column=7).value = men_num
+        except Exception as e:
+            return "error in countPatients:"+str(e)
        
         wb.save("records/medical.xlsx") #update excel file
         return records
    
     def isAlredySaved(id, name, birthday):
-        #to get list of saved names and birthdays
-        df=pd.read_excel('records/medical.xlsx', id) 
-        name_list = df["ФИО пациента"].tolist() 
-        birth_list = df["Дата рождения"].tolist() 
+        try:
+            #to get list of saved names and birthdays
+            df=pd.read_excel('records/medical.xlsx', id) 
+            name_list = df["ФИО пациента"].tolist() 
+            birth_list = df["Дата рождения"].tolist() 
         
-        if(name in name_list): # check if name already exists
-            index = name_list.index(name)
-            if(birth_list[index]==birthday): #check if birthday is the same
-                return True
-        else:
-            return False   
+            if(name in name_list): # check if name already exists
+                index = name_list.index(name)
+                if(birth_list[index]==birthday): #check if birthday is the same
+                    return True
+            else:
+                return False  
+        except Exception as e:
+            return "error in isAlredySaved:"+str(e) 
         
     def checkSavePatientGetPage(self,
                                 id,
@@ -90,17 +104,20 @@ class Service():
                                 patient_birthdate, 
                                 patient_reason,
                                 patient_pressure):
-        patient_time = datetime.now()
-        wb = self.getWB()
-        isUnique = self.isAlredySaved(id, patient_name, patient_birthdate)
-        if(isUnique):
-            return ("/day/"+id+"/1/"+patient_name)
-        elif (id in wb.sheetnames):
-            new_data = [patient_id, patient_time, patient_name, patient_type, patient_birthdate, patient_reason, patient_pressure]
-            self.saveRecord(wb, id, new_data)
-            return ("/day/"+id)
-        else:
-            return ("/")    
+        try:
+            patient_time = datetime.now()
+            wb = self.getWB()
+            isUnique = self.isAlredySaved(id, patient_name, patient_birthdate)
+            if(isUnique):
+                return ("/day/"+id+"/1/"+patient_name)
+            elif (id in wb.sheetnames):
+                new_data = [patient_id, patient_time, patient_name, patient_type, patient_birthdate, patient_reason, patient_pressure]
+                self.saveRecord(wb, id, new_data)
+                return ("/day/"+id)
+            else:
+                return ("/")  
+        except Exception as e:
+            return "error in checkSavePatientGetPage:"+str(e)  
                      
                 
 
