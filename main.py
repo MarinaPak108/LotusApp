d9feb23a6a8b272f129e6ebae1e4ce25bb75a70f
pakmarina108@gmail.com <pm@mail.ru>
2024-05-31 18:54:33 +0300

    added saving files in by day folder with logger, added more logs

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import os
 from flaskwebgui import FlaskUI
-from datetime import date, datetime
 from flask import Flask, redirect, request, render_template
+import logging   
 
 from models.doctor import Doctor
 from models.patient import Patient
-from models.record import Record
 from service.service import Service
 
 app = Flask(__name__)
+layer = "=============> Controller"
+day = Service.getDay()
+wb = Service.getWB()
+folder_path =os.path.join("D:\LotusApp\records", day)
 
-directory = os.getcwd()
-layer = "Controller"
 @app.route("/")
 def home():
     try: 
-        day = Service.getDay()
-        wb = Service.getWB()
         printErrorInLoggerThrowException(wb)
         names = wb.sheetnames
         if (day not in names):
-            app.logger.info('%s : day - %s', layer, day)
+            app.logger.info('%s def %s: day - %s', layer, request.endpoint,  day)
+            previous_path = os.path.join("D:\LotusApp\records", names[-1])
+            if(os.path.isdir(previous_path)):
+                wb.save(previous_path)
             return redirect('/assign')
         else:
             ws = wb["current"]
@@ -31,36 +33,32 @@ def home():
             return render_template('start.html', title="Список отчетов", tab = header, records = records)
         #raise Exception("error in getWB:")
     except Exception as e:
-            return redirectToErrorPage(str(e), __name__)
+            return redirectToErrorPage(str(e), "def "+request.endpoint)
        
 @app.route('/assign')
 def my_form():
     try:
-        day = Service.getDay()
-        wb = Service.getWB()
         printErrorInLoggerThrowException(wb)
         isButtonActive=False
         ws = wb['settings']
         doctors = Service.fromExcelToList(Doctor, ws)
         printErrorInLoggerThrowException(doctors)
+        app.logger.info('%s def %s : number of doctors in %s equals %s', layer, request.endpoint, day, len(doctors))
         if (day not in wb.sheetnames):
             isButtonActive = True
         return render_template('assign.html', doctors = doctors, isActive = isButtonActive)
     except Exception as e:
-            return redirectToErrorPage(str(e), __name__)
+            return redirectToErrorPage(str(e), "def "+request.endpoint)
         
 @app.route('/assign', methods=['POST'])
 def my_form_post():
     try:
         docId = request.form['text']
-        day = Service.getDay()
-
-        wb = Service.getWB()
         printErrorInLoggerThrowException(wb)
         ws_new = wb.create_sheet(day) 
         ws_header = ["Время","ФИО пациента", "М\Ж\Р", "Дата рождения", "Причина", "Давление"]
         ws_new.append(ws_header)
-        app.logger.info('%s : added worksheet (%s)', layer, day)
+        app.logger.info('%s def %s : added worksheet (%s)', layer,request.endpoint ,day)
         
         wsDoc = wb["settings"]
         docName = wsDoc[docId][1].value
@@ -69,28 +67,29 @@ def my_form_post():
         ws = wb["current"]
         ws.append(new_data)
         wb.save("records/medical.xlsx")
-        app.logger.info('%s : saved doctor (%s) name - %s', layer, docId, docName)
-        app.logger.info('%s : saved document with new data', layer)
+        app.logger.info('%s def %s : saved doctor (%s) name - %s on day %s', layer,request.endpoint, docId, docName, day)
+        app.logger.info('%s def %s: saved document with new data', layer,request.endpoint)
         return redirect("/")
     except Exception as e:
-        return  redirectToErrorPage(str(e), __name__)
+        return  redirectToErrorPage(str(e), "def "+request.endpoint)
         
 @app.route('/day/<id>', defaults={'errid': 0, 'name': None})
 @app.route('/day/<id>/<errid>/<name>')
 def patients(id, errid, name):
     try:
         error = ""
-        wb = Service.getWB()
         printErrorInLoggerThrowException(wb)
         isActiveDay = False
-        if(id == Service.getDay):
+        if(id == day):
             isActiveDay = True
         if(errid=="1"):
+            app.logger.info('%s def %s : patient %s already exists', layer, request.endpoint , name)
             error = "ФИО "+name+" с идентичной датой рождения уже существует. Пожалуйста проверьте еще раз данные."
         if(id in wb.sheetnames):
             ws = wb[id]  
             patients= Service.fromExcelToList(Patient, ws)
             printErrorInLoggerThrowException(patients)
+            app.logger.info('%s def %s : number of patients in %s equals %s', layer, request.endpoint, id, len(patients))
             return render_template('patient.html', 
                                     patients = patients, 
                                     day = id, 
@@ -100,7 +99,7 @@ def patients(id, errid, name):
         else:
             return redirect("/")
     except Exception as e:
-            return  redirectToErrorPage(str(e), __name__)
+            return  redirectToErrorPage(str(e), "def "+request.endpoint)
         
 @app.route('/day/<id>', methods=['POST'])
 def patients_post(id):
@@ -120,9 +119,10 @@ def patients_post(id):
                                             patient_reason,
                                             patient_pressure)
         printErrorInLoggerThrowException(url)
+        app.logger.info('%s def %s : patient %s_%s saved to excel', layer, request.endpoint , patient_id, patient_name)
         return redirect(url)
     except Exception as e:
-           return  redirectToErrorPage(str(e), __name__)
+           return  redirectToErrorPage(str(e), "def "+request.endpoint)
 
 @app.route('/day/<id>/<errid>/<name>', defaults={'errid': 0, 'name': None},  methods=['POST'])
 def patients_post_error(id):
@@ -142,9 +142,10 @@ def patients_post_error(id):
                                             patient_reason,
                                             patient_pressure)
         printErrorInLoggerThrowException(url)
+        app.logger.info('%s def %s : patient %s_%s saved to excel', layer, request.endpoint , patient_id, patient_name)
         return redirect(url)
     except Exception as e:
-            redirectToErrorPage(str(e), __name__)
+            redirectToErrorPage(str(e), "def "+request.endpoint)
 
 #################################################################################
 #to print error msg from service layer and then redirect via Exception to error.html page
@@ -158,7 +159,12 @@ def redirectToErrorPage(msg, name):
     return render_template('error.html')
 ################################################################################### 
 if __name__ == "__main__":
-    import logging   
-    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+    
+    #create folder for today
+    if(os.path.isdir(folder_path)==False):
+        os.makedirs(folder_path)
+    #save logs file to today folder
+    logfile = os.path.join(folder_path, 'debug.log')
+    logging.basicConfig(filename=logfile,level=logging.DEBUG)
     FlaskUI(app=app, server="flask",  width= 800, height=600).run()
   
